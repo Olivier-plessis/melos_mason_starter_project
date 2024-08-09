cb0cc8c7740e89a3460fc1cba0356eb88e3add04
olivier_plessis <hello@olivierplessis.fr>
2024-08-09 07:32:46 +0200

    empty model

--- bricks/feature_brick/__brick__/{{feature_name.snakeCase()}}/domain/model/{{feature_name.snakeCase()}}.dart
+++ bricks/feature_brick/__brick__/{{feature_name.snakeCase()}}/domain/model/{{feature_name.snakeCase()}}.dart
@@ -10,5 +10,10 @@ class {{feature_name.pascalCase()}} with _${{feature_name.pascalCase()}} {
   // TODO add params here
   ) = _{{feature_name.pascalCase()}};
   factory {{feature_name.pascalCase()}}.fromJson(Map<String, Object?> json) => _${{feature_name.pascalCase()}}FromJson(json);
+
+    factory {{feature_name.pascalCase()}}.empty() => _{{feature_name.pascalCase()}}(
+    // TODO add params here
+  );
+  
 }
 {{/freezed}}
